5b6c0736d23b4fff7eca89ec4111f9960d863457
Vlatko Vlahek <vlatko@prototyp.digital>
2022-11-29 16:24:59 +0100

    A minimum viable example.

---
+++ data.tf
@@ -0,0 +1,17 @@
+data "aws_availability_zones" "available" {}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+  owners = ["amazon"]
+}
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  sensitive   = true
+}
+
+variable "aws_access_secret" {
+  description = "AWS Access Secret"
+  sensitive   = true
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+}
